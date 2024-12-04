c01c729dd61f0c9dad4133d682041cc611492e95
hajoeun <hajoeun90@gmail.com>
2024-12-04 23:14:01 +0900

    fix - next config

--- next.config.mjs
+++ next.config.mjs
@@ -40,4 +40,29 @@ export default withMDX({
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'puppeteer', 'puppeteer-core'];
+    }
+
+    // Ignore source map files
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      loader: 'ignore-loader',
+    });
+
+    // Add alias for puppeteer modules
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'puppeteer/lib/cjs/puppeteer/common/Browser':
+        'puppeteer-core/lib/cjs/puppeteer/common/Browser',
+      'puppeteer/lib/cjs/puppeteer/common/Page': 'puppeteer-core/lib/cjs/puppeteer/common/Page',
+      'puppeteer/lib/cjs/puppeteer/common/FrameManager':
+        'puppeteer-core/lib/cjs/puppeteer/common/FrameManager',
+      'puppeteer/lib/cjs/puppeteer/common/JSHandle':
+        'puppeteer-core/lib/cjs/puppeteer/common/JSHandle',
+    };
+
+    return config;
+  },
 });
